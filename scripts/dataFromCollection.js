b9cbdddb10b58c598fb4b97a5f2a0d15963f2729
Koen Derks <koen-derks@hotmail.com>
2023-05-12 20:21:01 +0200

    Update dataFromCollection.js

--- scripts/dataFromCollection.js
+++ scripts/dataFromCollection.js
@@ -4,7 +4,11 @@ function dataFromCollection() {
 	var numericCells = document.getElementsByClassName('MuiDataGrid-cellContent');
 	var stringCells = document.getElementsByClassName('css-j7qwjs');
 	if (numericCells.length > 0 && rows.length > 0) {
-		const aggregated = [['Number', 'Title', 'ID', 'Type', "Score", "LearningObjective", "Taxonomy"]];
+		const aggregated = [['Number', 'Title', 'ID', 'Type', "Score"]];
+		if (stringCells.length > 0) {
+			aggregated[0].push("LearningObjective");
+			aggregated[0].push("Taxonomy");
+		}
 		var numericIndex = 0;
 		var stringIndex = 0;
 		for (let i = 0; i < rows.length; i++) {
