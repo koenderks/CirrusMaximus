4d38553164a0363f1fc8d040ba28ec2cd3fd1ab5
Koen Derks <koen-derks@hotmail.com>
2023-05-12 12:56:42 +0200

    Update common.js

--- scripts/common.js
+++ scripts/common.js
@@ -1,18 +1,17 @@
 function exportToCsv(filename, rows) {
 	const csvContent = rows.map(row => row.join(';')).join('\n');
-	const blob = new Blob([csvContent], { type: 'text/csv' });
+	const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
 	const url = URL.createObjectURL(blob);
-
 	const link = document.createElement('a');
 	link.setAttribute('href', url);
 	link.setAttribute('download', filename);
+	link.style.display = 'none';
+	document.body.appendChild(link);
 	link.click();
+	document.body.removeChild(link);
 }
 
 function getUniqueElements(arr, colIndex) {
-	const uniqueElements = new Set();
-	for (let i = 0; i < arr.length; i++) {
-		uniqueElements.add(arr[i][colIndex]);
-	}
+	const uniqueElements = new Set(arr.map(item => item[colIndex]));
 	return Array.from(uniqueElements);
 }
