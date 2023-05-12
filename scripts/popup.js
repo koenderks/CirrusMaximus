64109774a4637a2d5cb40bc3212f8f03d7b6c78c
Koen Derks <koen-derks@hotmail.com>
2023-05-12 13:01:05 +0200

    Update popup.js

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,96 +1,64 @@
 // Check the current url and hide elements accordingly
-chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
-	var currentUrl = tabs[0].url;
+chrome.tabs.query({ active: true, lastFocusedWindow: true }, async (tabs) => {
+	const currentUrl = tabs[0].url;
 	const par = document.getElementById("status");
 	if (currentUrl.includes("cirrusplatform.com/author/app#/library/collections/")) {
-		par.innerHTML = "Status: <b style='color: green;'>Active</b>";
-		const buttons = document.querySelectorAll(".form-button");
-		buttons.forEach(button => button.classList.add("hidden"));
+		par.innerHTML = `Status: <b style='color: green;'>Active</b>`;
+		hideButtonsByClass("form-button");
 	} else if (currentUrl.includes("cirrusplatform.com/author/app#/assessments/item/")) {
-		par.innerHTML = "Status: <b style='color: green;'>Active</b>";
-		const buttons = document.querySelectorAll(".collection-button");
-		buttons.forEach(button => button.classList.add("hidden"));
+		par.innerHTML = `Status: <b style='color: green;'>Active</b>`;
+		hideButtonsByClass("collection-button");
 	} else {
-		const collectionButtons = document.querySelectorAll(".collection-button");
-		collectionButtons.forEach(button => button.classList.add("hidden"));
-		const formButtons = document.querySelectorAll(".form-button");
-		formButtons.forEach(button => button.classList.add("hidden"));
+		hideButtonsByClass("collection-button");
+		hideButtonsByClass("form-button");
 	}
 });
 
-injectMatrixFromCollectionScript = (tab) => {
-	const { id, url } = tab;
-	chrome.scripting.executeScript(
-		{
-			target: { tabId: id, allFrames: true },
-			files: ['./scripts/common.js', './scripts/matrixFromCollection.js']
-		}
-	)
-}
+const hideButtonsByClass = (className) => {
+	const buttons = document.getElementsByClassName(className);
+	for (let i = 0; i < buttons.length; i++) {
+		buttons[i].classList.add("hidden");
+	}
+};
 
-injectMatrixFromFormScript = (tab) => {
-	const { id, url } = tab;
-	chrome.scripting.executeScript(
-		{
-			target: { tabId: id, allFrames: true },
-			files: ['./scripts/common.js', './scripts/matrixFromForm.js']
-		}
-	)
-}
+const executeScriptInTab = async (tab, files) => {
+	await chrome.scripting.executeScript({
+		target: { tabId: tab.id, allFrames: true },
+		files: ['./scripts/common.js', ...files],
+	});
+};
 
-injectDataFromCollectionScript = (tab) => {
-	const { id, url } = tab;
-	chrome.scripting.executeScript(
-		{
-			target: { tabId: id, allFrames: true },
-			files: ['./scripts/common.js', './scripts/dataFromCollection.js']
-		}
-	)
-}
-
-injectDataFromFormScript = (tab) => {
-	const { id, url } = tab;
-	chrome.scripting.executeScript(
-		{
-			target: { tabId: id, allFrames: true },
-			files: ['./scripts/common.js', './scripts/dataFromForm.js']
-		}
-	)
-}
-
-getCurrentChromeTab = async () => {
-	let queryOptions = { active: true };
-	let [tab] = await chrome.tabs.query(queryOptions);
+const getCurrentChromeTab = async () => {
+	const [tab] = await chrome.tabs.query({ active: true });
 	return tab;
-}
+};
 
 document.addEventListener("DOMContentLoaded", () => {
 	// Inject script to create assessment matrix from collection on click
 	const collectionMatrixButton = document.getElementById("collectionMatrixButton");
-	collectionMatrixButton.addEventListener("click", () => {
-		getCurrentChromeTab().then((tab) => {
-			injectMatrixFromCollectionScript(tab)
-		})
+	collectionMatrixButton.addEventListener("click", async () => {
+		const tab = await getCurrentChromeTab();
+		await executeScriptInTab(tab, ['./scripts/matrixFromCollection.js']);
 	});
+
 	// Inject script to create assessment matrix from form on click
 	const formMatrixButton = document.getElementById("formMatrixButton");
-	formMatrixButton.addEventListener("click", () => {
-		getCurrentChromeTab().then((tab) => {
-			injectMatrixFromFormScript(tab)
-		})
+	formMatrixButton.addEventListener("click", async () => {
+		const tab = await getCurrentChromeTab();
+		await executeScriptInTab(tab, ['./scripts/matrixFromForm.js']);
 	});
+
 	// Inject script to extract collection data on click
 	const collectionDataButton = document.getElementById("collectionDataButton");
-	collectionDataButton.addEventListener("click", () => {
-		getCurrentChromeTab().then((tab) => {
-			injectDataFromCollectionScript(tab)
-		})
+	collectionDataButton.addEventListener("click", async () => {
+		const tab = await getCurrentChromeTab();
+		await executeScriptInTab(tab, ['./scripts/dataFromCollection.js']);
 	});
+
 	// Inject script to extract form data on click
 	const formDataButton = document.getElementById("formDataButton");
-	formDataButton.addEventListener("click", () => {
-		getCurrentChromeTab().then((tab) => {
-			injectDataFromFormScript(tab)
-		})
+	formDataButton.addEventListener("click", async () => {
+		const tab = await getCurrentChromeTab();
+		await executeScriptInTab(tab, ['./scripts/dataFromForm.js']);
 	});
 });
